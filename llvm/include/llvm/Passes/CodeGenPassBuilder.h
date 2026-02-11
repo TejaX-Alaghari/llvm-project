bb9b5bef90b74e4fb8bddfba847b0694a864b9be
tmahatej_amdeng <teja.alaghari@amd.com>
2026-02-11 07:33:41 +0530

    Fixes to be upstreamed - 1

--- llvm/include/llvm/Passes/CodeGenPassBuilder.h
+++ llvm/include/llvm/Passes/CodeGenPassBuilder.h
@@ -501,6 +501,8 @@ protected:
   /// Add standard basic block placement passes.
   void addBlockPlacement(PassManagerWrapper &PMW) const;
 
+  void addPostBBSections(PassManagerWrapper &PMW) const {}
+
   using CreateMCStreamer =
       std::function<Expected<std::unique_ptr<MCStreamer>>(MCContext &)>;
   void addAsmPrinter(PassManagerWrapper &PMW, CreateMCStreamer) const {
@@ -1074,6 +1076,8 @@ Error CodeGenPassBuilder<Derived, TargetMachineT>::addMachinePasses(
       addModulePass(MachineOutlinerPass(Opt.EnableMachineOutliner), PMW);
     }
   }
+
+  derived().addPostBBSections(PMW);
 
   addMachineFunctionPass(StackFrameLayoutAnalysisPass(), PMW);
 
